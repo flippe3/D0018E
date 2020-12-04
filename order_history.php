0135cd9f2c0031ce4fd234a5407d20bae0cd252d
Filip Rpi <filip.c.nilsson@gmail.com>
2020-12-04 14:35:47 +0000

    added orderhistory

--- order_history.php
+++ order_history.php
@@ -8,7 +8,7 @@ print_r($_SESSION);
 	<link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
-  <a href='index.php'><header style="color: magenta;">Shopping cart</header></a>
+  <a href='index.php'><header style="color: green;">Order History</header></a>
  <?php
    if($_SESSION["login_status"] === true){
        echo <<<_END
@@ -40,28 +40,34 @@ print_r($_SESSION);
       <div id="tablediv">
 	  <table border="1" align="left">
 	<tr id="head_column">
-	  <td>Image</td>
+	  <td>Orderid</td>
+	  <td>Date</td>
 	  <td>ISBN</td>
 	  <td>Title</td>
-	  <td>Author</td>
 	  <td>Price</td>
-	  <td>Year</td>
 	  <td>Quantity</td>
-	  <td>Remove from cart</td>
+	  <td>Leave Review</td>
 	</tr>
     <?php
 
           $userid = $_SESSION['userID'];
-          $get_orderid = mysqli_fetch_assoc(mysqli_query($dbconnect, "SELECT orderid FROM Orders where (customerid='$userid' and active=1)")) or die(mysqli_error($link));
-          $orderid = $get_orderid['orderid'];
-          $get_orderlist = mysqli_query($dbconnect, "SELECT * FROM Orderlist where orderid='$orderid'") or die(mysqli_error($link));
+$fetch_order = mysqli_query($dbconnect, "SELECT orderid FROM Orders where (customerid='$userid' and active=0)") or die(mysqli_error($dbconnect));
+
+
+while($get_orderid = mysqli_fetch_array($fetch_order)){
+$orderid = $get_orderid['orderid'];
+          $get_orderlist = mysqli_query($dbconnect, "SELECT * FROM Orderlist where orderid='$orderid'") or die(mysqli_error($dbconnect));
+          
           $total_price = 0;
           $total_quantity = 0;
+  	 while ($row = mysqli_fetch_array($get_orderlist)) {
+         $date = mysqli_fetch_assoc(mysqli_query($dbconnect, "SELECT transactiondate FROM Orders where orderid='$orderid'")) or die(mysqli_error($dbconnect));
+         $printdate = $date['transactiondate'];
+         
 
-	 while ($row = mysqli_fetch_array($get_orderlist)) {
          $isbn = $row['isbn'];
-         $get_book = mysqli_fetch_assoc(mysqli_query($dbconnect, "SELECT * FROM Products where isbn='$isbn'")) or die(mysqli_error($link));
-         $get_quantity = mysqli_fetch_assoc(mysqli_query($dbconnect, "SELECT quantity FROM Orderlist where (isbn='$isbn' and orderid='$orderid')")) or die(mysqli_error($link));
+         $get_book = mysqli_fetch_assoc(mysqli_query($dbconnect, "SELECT * FROM Products where isbn='$isbn'")) or die(mysqli_error($dbconnect));
+         $get_quantity = mysqli_fetch_assoc(mysqli_query($dbconnect, "SELECT quantity FROM Orderlist where (isbn='$isbn' and orderid='$orderid')")) or die(mysqli_error($dbconnect));
          $book_price = $get_book['price'] * $get_quantity['quantity'];
          $total_price = $total_price + $book_price;
          $total_quantity = $total_quantity + $get_quantity['quantity'];
@@ -71,57 +77,22 @@ print_r($_SESSION);
 		 }
          echo
 	 "<tr id='entries'>
-	<td><img id='table_image' src=$imageurl></td>
+	<td>{$orderid}</td>
+	<td>{$printdate}</td>
 	<td>{$get_book['isbn']}</td>
 	<td>{$get_book['title']}</td>
-	<td>{$get_book['author']}</td>
 	<td>{$book_price} kr</td>
-	<td>{$get_book['releaseyear']}</td>
 	<td>{$get_quantity ['quantity']}</td>
     <form method='POST' action='server/remove_from_cart.php'>
-	<td><button id='btn_remove' type='submit' value='$isbn' name='isbn'>Remove</button></td>
+	<td><button id='btn_grade' type='submit' value='$isbn' name='isbn'>Grade</button></td>
     </form>
     </tr>\n";}
-    echo "
-	<tr id='footer_column'>
-	  <td></td>
-	  <td></td>
-	  <td></td>
-	  <td></td>
-	  <td>Sum</td>
-	  <td></td>
-	  <td>Total Quantity</td>
-	  <td></td>
-	</tr>
-   <tr id='footer_column'>
-	  <td></td>
-	  <td></td>
-	  <td></td>
-	  <td></td>
-	  <td>{$total_price}</td>
-	  <td></td>
-	  <td>{$total_quantity}</td>
-	  <td></td>
-	</tr>";
-   ?>
+
+}
+?>
 
   </table>
   </div>
-  <div class="Orderform">
-   <h1 class="header">Order Form</h1> 
-  <form method="post" action="server/order.php">
-   <div class="input-group">
-     <label>Address</label>
-     <input type="text" name="address">
-   </div>
-   <div class="input-group">
-     <label>Zip</label>
-     <input type="text" name="zip">
-   </div>
-   <div class="input-group">
-     <button type="submit" class="btn" name="reg_user">Place Order</button>
-   </div>
-  </form>
 </div>        
 </div>
 </body>
