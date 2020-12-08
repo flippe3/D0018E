f2cdc498d95e0ef6c422726f7e47064bc2523882
Filip Rpi <filip.c.nilsson@gmail.com>
2020-12-08 13:37:56 +0000

    fixed empty reviews

--- server/submit_review.php
+++ server/submit_review.php
@@ -11,13 +11,21 @@ $isbn = $_REQUEST['isbn'];
 $grade = $_REQUEST['grade'];
 $review = mysqli_real_escape_string($link, $_REQUEST['review']);
 
-// Attempt insert query execution
-$submit = "INSERT INTO Reviews (isbn, grading, comment) VALUES ('$isbn', '$grade', '$review')";
-mysqli_query($link, $submit) or die(mysqli_error($link));
-
-echo '<script>alert("Submitted review.");
+if($grade == ''){
+    echo '<script>alert("You need to submit a grade.");
+      window.location.href="../order_history.php";          
+      </script>';    
+}
+else{
+    // Attempt insert query execution
+    $submit = "INSERT INTO Reviews (isbn, grading, comment) VALUES ('$isbn', '$grade', '$review')";
+    mysqli_query($link, $submit) or die(mysqli_error($link));
+    echo '<script>alert("Submitted review.");
       window.location.href="../order_history.php";          
       </script>';
+
+}
+
  
 // Close connection
 mysqli_close($link);
