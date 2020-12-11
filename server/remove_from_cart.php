3f498a5dee2b09fa0c4db9cb5c7bf992fa3394c9
Filip Rpi <filip.c.nilsson@gmail.com>
2020-12-11 13:10:25 +0000

    added transactions to remove_from_cart

--- server/remove_from_cart.php
+++ server/remove_from_cart.php
@@ -15,6 +15,9 @@ if(!isset($_SESSION["userID"])){
 // Escape user inputs for
 $isbn = mysqli_real_escape_string($link, $_REQUEST['isbn']);
 $userid = $_SESSION['userID'];
+
+mysqli_query($link, "START TRANSACTION");
+
 $get_orderid = mysqli_fetch_assoc(mysqli_query($link, "SELECT orderid FROM Orders where (customerid='$userid' and active=1)")) or die(mysqli_error($link));
 $orderid = $get_orderid['orderid'];
 
@@ -29,6 +32,8 @@ else{
     mysqli_query($link, "UPDATE Orderlist SET quantity=quantity - 1 WHERE (orderid='$orderid' and isbn='$isbn')") or die(mysqli_error($link));
 }
 echo '<script>window.location.href="../cart.php"</script>';
+
+mysqli_query($link, "COMMIT");
 
 // Close connection
 mysqli_close($link);
